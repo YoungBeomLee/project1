00c529509b042a2d46938e022629f512a949411d
YoungBeomLee <edlee127127@gmail.com>
2022-12-22 18:26:03 +0900

    slideToggle

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
 document.querySelector("#layer button").addEventListener("click", function () {
     document.querySelector("#layer").style.display = "none";
   });
-
+});
   $(function(){
   $(".family_site").click(function(){
-  $(".family_collection").show();
+  $(".family_collection").slideToggle();
   });
 });
