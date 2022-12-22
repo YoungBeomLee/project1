e1e09bec0010d25bd574e196c2496c0ef4862dcd
YoungBeomLee <edlee127127@gmail.com>
2022-12-22 18:21:16 +0900

    footer coo_info2 완료

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.querySelector("#layer button").addEventListener("click", function () {
     document.querySelector("#layer").style.display = "none";
   });
+
+  $(function(){
+  $(".family_site").click(function(){
+  $(".family_collection").show();
+  });
+});
