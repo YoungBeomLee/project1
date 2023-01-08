41c8c78a68dd0388a72f45fe89e1aaccf63956bf
YoungBeomLee <edlee127127@gmail.com>
2023-01-08 23:31:36 +0900

    일단 들고가보자

--- js/script_section3slide.js
+++ js/script_section3slide.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-	
+	document.querySelector("#layer button").addEventListener("click", function () {
+		document.querySelector("#layer").style.display = "none";
+	});
 });
 
 $(function () {
