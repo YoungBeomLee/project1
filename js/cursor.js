5ad655907dfba6d76150dcd1a1cf43a630acb4d8
YoungBeomLee <edlee127127@gmail.com>
2023-01-12 18:17:20 +0900

    01-12 커서수업 적용

---
+++ js/cursor.js
@@ -0,0 +1,10 @@
+const cursor=$(".cursor_image");
+$(window).mousemove(function (e) {
+    const pointer = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+  
+    cursor.css({"top":pointer.y,"left":pointer.x,"margin-left":-pointer.x/2,"margin-top":-pointer.y/2})
+
+  });
