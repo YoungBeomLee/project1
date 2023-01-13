44980f08beda1921ccb8c74665d7902887b21a0e
YoungBeomLee <edlee127127@gmail.com>
2023-01-13 17:35:18 +0900

    커서포인터넣기

--- js/cursor.js
+++ js/cursor.js
@@ -1,10 +1,26 @@
-const cursor=$(".cursor_image");
-$(window).mousemove(function (e) {
-    const pointer = {
-      x: e.clientX,
-      y: e.clientY,
-    };
-  
-    cursor.css({top:pointer.y,left:pointer.x,"margin-left":-pointer.x/2,"margin-top":-pointer.y/2})
+document.addEventListener("DOMContentLoaded", function () {
+  const cursor = document.querySelector(".cursor_image");
+  const cStyle = window.getComputedStyle(cursor);
+  console.log(cStyle.width.split("px", "1"));
 
+  addEventListener("mousemove", function (e) {
+    //매개변수로 윈도우 객체 받기
+    /*마우스가 움직일때 포인터의 좌표값 얻기
+      얻어낸 좌표를 dom에 할당
+      */
+    let pointer = e;
+    console.log(pointer);
+    let pointerlocation = [pointer.clientX, pointer.clientY];
+    let pObj = {
+      x: pointer.clientX / 2 + "px",
+      y: pointer.clientY / 3 + "px",
+    };
+    let cSize = [cStyle.width.split("px", "1"), cStyle.height.split("px", "1")];
+    cursor.style.marginLeft = -7 + "px";
+    cursor.style.marginTop = -7 + "px";
+    cursor.style.left = pointerlocation[0] + "px";
+    cursor.style.top = pointerlocation[1] + "px";
+
+    //console.log(cSize);
   });
+});
